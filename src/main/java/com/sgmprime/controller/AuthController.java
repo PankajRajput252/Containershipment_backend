e3c95c44e0825d0547b35d7952b41f3801792f89
Pankaj Kumar Singh <pankaj.singh@tapplent.com>
2026-03-24 14:51:10 +0530

    login

--- src/main/java/com/sgmprime/controller/AuthController.java
+++ src/main/java/com/sgmprime/controller/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/v1/auth/")
+@RequestMapping("/api/v1/auth")
 @CrossOrigin("*")
 public class AuthController {
 
